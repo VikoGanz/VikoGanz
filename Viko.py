592aa1734d852e9bdfb27bc9bc6d03087e4a7b93
VikoGanz <93149749+VikoGanz@users.noreply.github.com>
2021-10-26 05:20:37 +0700

    Update and rename README.md to Viko.py

---
+++ Viko.py
@@ -0,0 +1,15 @@
+#install
+
+pkg update && pkg upgrade
+
+pkg install python2
+
+pkg install git
+
+git clone https://github.com/vikoganz/Viko
+
+cd Viko
+
+pip2 install -r zona.Viko
+
+python2 Viko.py
